006d5fe1827b24416f756147cd6c8239bb5213cd
Luizerz <senaluiz1307@gmail.com>
2024-01-01 14:38:52 -0300

    add JS

--- src/script.js
+++ src/script.js
@@ -1,1 +1,32 @@
-console.log('FUNCIONANDO')
+let sliderElement = document.querySelector("#slider");
+let buttomElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#value");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+"
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function() {
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyToClipBoard() {
+    navigator.clipboard.writeText(novaSenha);
+    alert("Senah copiada com sucesso!");   
+}
